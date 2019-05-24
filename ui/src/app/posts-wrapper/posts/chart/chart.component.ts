da9fa283be67ae7900c09ed5135f643ef88fad13
oz izhak <ozizhak5540@gmail.com>
2019-05-24 14:56:25 +0300

    fix chart sort

--- ui/src/app/posts-wrapper/posts/chart/chart.component.ts
+++ ui/src/app/posts-wrapper/posts/chart/chart.component.ts
@@ -30,12 +30,13 @@ export class ChartComponent implements OnInit, OnDestroy {
   ngOnInit() {
     this.utilsService.chartHasChanged.subscribe(
       posts => {
+        debugger
         this.single = this.utilsService.setChart(posts);
         // SORT BY HOUR
         this.single.sort((a, b) => {
-          if (a.name > b.name) {
+          if (a['name'] > b['name']) {
             return 1;
-          } else if (a.name < b.name) {
+          } else if (a['name'] < b['name']) {
             return -1;
           } else {
             return 0;
